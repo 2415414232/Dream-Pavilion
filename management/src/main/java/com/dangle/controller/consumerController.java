d939e5157964945056aece6f6d91cb4e27f0de48
dzl <2415414232@qq.com>
2019-09-30 15:46:31 +0800

    客户模块

---
+++ management/src/main/java/com/dangle/controller/consumerController.java
@@ -0,0 +1,13 @@
+package com.dangle.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author by 党桢乐 on 2019/9/30
+ */
+@RestController
+@RequestMapping("/consumer")
+public class consumerController {
+
+}
